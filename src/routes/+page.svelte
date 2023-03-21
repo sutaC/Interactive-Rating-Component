f5bcbfc4180ad44dd9b3df238ed49ff19fb5d434
Antoni Siekański <catus.pl@gmail.com>
2023-03-21 12:41:18 +0100

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+
+</script>
+<!-- --- -->
+
+
+
+
+
+<footer>
+Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+Coded by <a href="#sutaC">sutaC</a>.
+</footer>
+
+<!-- --- -->
+<style>
+
+
+
+
+    footer {
+        position: fixed;
+        width: 100%;
+        text-align: center;
+        bottom: 0;
+        tab-size: 10px;
+    }
+
+</style>
